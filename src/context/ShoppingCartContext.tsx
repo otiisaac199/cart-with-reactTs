261f5dd9b9564b32d621d9033af87d28a2bdb73f
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-25 22:41:26 +0100

    feat: created a context for the cart and added function types for the cart functionality

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export const useShoppingCart = () => {
+  return useContext(ShoppingCartContext);
+};
+
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  return (
+    <ShoppingCartContext.Provider value={{ getItemQuantity }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
