a86cf6ef75ca736b348c8317fab87611d114cb69
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-25 22:03:27 +0100

    feat: created the store item cards

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import StoreItem from "../components/StoreItem";
+
 const Store = () => {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item, index) => (
+          <Col key={index}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
