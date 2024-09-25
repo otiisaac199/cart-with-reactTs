bf89f855c5d3db207792aeb213189428f7d3d9d3
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-25 21:45:32 +0100

    feat: created a sticky navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import { ShoppingCartIcon } from "lucide-react";
+import { Button, Container, Nav, Navbar as NavBarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <NavBarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <ShoppingCartIcon style={{ width: "1.5rem", height: "2rem" }} />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            0
+          </div>
+        </Button>
+      </Container>
+    </NavBarBs>
+  );
+};
+
+export default Navbar;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,5 @@
 const Store = () => {
-  return <div>Store</div>;
+  return <h1>Store</h1>;
 };
 
 export default Store;
