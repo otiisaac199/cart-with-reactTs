5c0960699acb7ba25517ff173b7e2c567a0724b4
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-26 12:16:17 +0100

    feat: cart total value done, working perfectly

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -2,6 +2,8 @@ import { Stack } from "react-bootstrap";
 import { Offcanvas } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import CartItem from "./CartItem";
+import formatCurrency from "../utilities/formatCurrency";
+import storeItems from "../data/items.json";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -20,6 +22,15 @@ const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
